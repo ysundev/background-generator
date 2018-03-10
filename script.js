0a6111c529a1200f3aac787763e9954696503d12
Yu Sun <yu.sun.dev@gmail.com>
2018-03-10 00:00:53 -0500

    Installed and test lodash:
    modified script.js
    browserify script.js > bundle.js
    modify index.html
    live-server

--- script.js
+++ script.js
@@ -1,30 +1,23 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var _ = require('lodash');
 
-function inputLength() {
-	return input.value.length;
+var array = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(array,3));
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
-}
+color1.addEventListener("input", setGradient);
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
+color2.addEventListener("input", setGradient);
